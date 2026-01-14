11928688d91da40bce41d4d6db39fff510106ff6
binnotkari-wq <benoit.dorczynski@gmail.com>
2026-01-14 23:47:30 +0100

    Delete Scripts directory

--- Scripts/stop_llm.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# On cherche et on tue le processus
-if pkill llama-server
-then
-    echo "LLM" "Le serveur a été arrêté."
-else
-    echo "LLM" "Aucun serveur n'était en cours d'exécution."
-fi

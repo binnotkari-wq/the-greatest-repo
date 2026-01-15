3d780dcbb4e4ce223a6593c90904e857aa9f081d
binnotkari-wq <benoit.dorczynski@gmail.com>
2026-01-15 10:00:36 +0100

    Update zimify.sh

--- Scripts/zimify.sh
+++ Scripts/zimify.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-# on veut ajouter un contenu (qui se trouve dans le fichier 00_entête.txt) au début de chaque fichiers textes d'un lot (ici, il s'agissait de fichiers texte résultant d'une conversion de l'export .html de NoteKeep (voir "Convertire HTML en TXT.md"). Cela permettra leur reconnaissance en tant que page dans Zim. Mais il faut au préalable nettoyer les noms de fichiers.
+# on veut ajouter un contenu (qui se trouve dans le fichier 00_entête.txt) au début de chaque fichiers
+# textes d'un lot (ici, il s'agissait de fichiers texte résultant d'une conversion de l'export .html de
+# NoteKeep (voir "Convertire HTML en TXT.md"). Cela permettra leur reconnaissance en tant que page dans Zim.
+# Mais il faut au préalable nettoyer les noms de fichiers.
 
 
 # préparation : création du dossier de sortie
 mkdir -p txt_entete_zim
 
-# instruction : "file" est le nom de chaque fichier txt existant dans le répertoire où on place le script, et qu'on veut renommer de façon valide pour Zim
+# instruction : "file" est le nom de chaque fichier txt existant dans le répertoire où on place le script,
+# et qu'on veut renommer de façon valide pour Zim
 for file in *.txt; do
 
 # suppression des espaces dans les noms de fichier, et remplacement par _ (sinon, seront ignorés par Zim)
@@ -16,13 +20,15 @@ done
 
 
 
-# instruction : "file" est le nom de chaque fichier txt existant (et renommé, à présent) dans le répertoire où on place le script, et qu'on veut faire reconnaitre par Zim en ajoutant l'entête-type de Zim.
+# instruction : "file" est le nom de chaque fichier txt existant (et renommé, à présent) dans 
+# le répertoire où on place le script, et qu'on veut faire reconnaitre par Zim en ajoutant l'entête-type de Zim.
 
 for file in *.txt; do
 # création d'un fichier vide dans le répertoire de sortie, portant le même nom que les fichiers originaux
 touch "txt_entete_zim/${file%.txt}.txt"
 
-# fusion du contenu de l'entête, avec le contenu de chaque fichier. Cette fusion est redirigée vers chaque fichier de sortie portant le même nom.
+# fusion du contenu de l'entête, avec le contenu de chaque fichier. Cette fusion est redirigée
+# vers chaque fichier de sortie portant le même nom.
 cat "00_entete.txt" "${file%.txt}.txt" > "txt_entete_zim/${file%.txt}.txt"
 
 done
